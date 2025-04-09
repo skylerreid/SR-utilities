e98b970d32bdc2b932e44e7c49bd995d20dfbf1a
Skyler Reid <96967651+skylerreid@users.noreply.github.com>
2025-04-09 11:05:42 -0600

    Update plot_waveforms_log.jl

--- plot_waveforms_log.jl
+++ plot_waveforms_log.jl
@@ -1,6 +1,6 @@
 using DataFrames, CSV, Statistics, Plots
 filename = "your_filepath_to_csv"
-data = CSV.read(filename, DataFrame; delim = ',', skipto=9)
+data = CSV.read(filename, DataFrame; delim = ',', skipto=9)  #skip the csv header containing metadata
 
 time_strings = data[9:end, 1]  #table gets read in as string instead of float
 voltage_strings = data[9:end, 2]
@@ -10,7 +10,7 @@ voltages = parse.(Float64, voltage_strings)
 
 #--------generate plot-----------
 mean_voltage = mean(voltages)
-line1 = mean_voltage + 0.01
+line1 = mean_voltage + 0.01 #plot additional lines around the mean if necessary
 line2 = mean_voltage - 0.01
 line3 = mean_voltage + 0.03
 line4 = mean_voltage - 0.03
