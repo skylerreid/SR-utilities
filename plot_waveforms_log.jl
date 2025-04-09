1d19ab26d7214e0e2cc98cb1b3caaefc15ff8133
Skyler Reid <96967651+skylerreid@users.noreply.github.com>
2025-04-09 10:17:42 -0600

    Create plot_waveforms_log.jl

---
+++ plot_waveforms_log.jl
@@ -0,0 +1,26 @@
+using DataFrames, CSV, Statistics, Plots
+filename = "your_filepath_to_csv"
+data = CSV.read(filename, DataFrame; delim = ',', skipto=9)
+
+time_strings = data[9:end, 1]  #table gets read in as string instead of float
+voltage_strings = data[9:end, 2]
+
+times = parse.(Float64, time_strings) #parse the strings to float64
+voltages = parse.(Float64, voltage_strings)
+
+#--------generate plot-----------
+mean_voltage = mean(voltages)
+line1 = mean_voltage + 0.01
+line2 = mean_voltage - 0.01
+line3 = mean_voltage + 0.03
+line4 = mean_voltage - 0.03
+
+plot(times, voltages, label="Output Voltage", color=:blue, linewidth=1.5, grid=true, gridlinewidth = 2)
+hline!([mean_voltage], linestyle=:dash, color=:black, label="Mean")
+hline!([line1, line2], linestyle=:dash, color=:red, label="±10mV")
+hline!([line3, line4], linestyle=:dash, color=:green, label="±30mV")
+
+# Formatting
+title!("Time Series Plot of Waveforms Data Log")
+xlabel!("Time (s)")
+ylabel!("Voltage (V)")
